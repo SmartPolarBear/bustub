05c3b285f3ada9ba3355ec80c5a35aed4d38e2ea
SmartPolarBear <clevercoolbear@outlook.com>
2024-07-18 23:41:01 +0800

    feat(project 2): implement internal page

--- src/include/storage/page/b_plus_tree_internal_page.h
+++ src/include/storage/page/b_plus_tree_internal_page.h
@@ -74,6 +74,13 @@ class BPlusTreeInternalPage : public BPlusTreePage {
   auto ValueAt(int index) const -> ValueType;
 
   /**
+   *
+   * @param index the index
+   * @param value the value to set
+   */
+  void SetValueAt(int index, const ValueType &value);
+
+  /**
    * @brief For test only, return a string representing all keys in
    * this internal page, formatted as "(key1,key2,key3,...)"
    *
--- src/storage/page/b_plus_tree_internal_page.cpp
+++ src/storage/page/b_plus_tree_internal_page.cpp
@@ -24,7 +24,16 @@ namespace bustub {
  * Including set page type, set current size, and set max page size
  */
 INDEX_TEMPLATE_ARGUMENTS
-void B_PLUS_TREE_INTERNAL_PAGE_TYPE::Init(int max_size) {}
+void B_PLUS_TREE_INTERNAL_PAGE_TYPE::Init(int max_size) {
+  SetPageType(IndexPageType::INTERNAL_PAGE);
+  SetMaxSize(max_size);
+  SetSize(1);
+
+  KeyType invalid_key{};
+  invalid_key.SetFromInteger(0);
+  SetKeyAt(0, invalid_key);
+  SetValueAt( 0, INVALID_PAGE_ID);
+}
 /*
  * Helper method to get/set the key associated with input "index"(a.k.a
  * array offset)
@@ -32,19 +41,32 @@ void B_PLUS_TREE_INTERNAL_PAGE_TYPE::Init(int max_size) {}
 INDEX_TEMPLATE_ARGUMENTS
 auto B_PLUS_TREE_INTERNAL_PAGE_TYPE::KeyAt(int index) const -> KeyType {
   // replace with your own code
-  KeyType key{};
+  BUSTUB_ASSERT(index >= 1 && index < GetSize(), "index out of range at KeyAt");
+  KeyType key{array_[index].first};
   return key;
 }
 
 INDEX_TEMPLATE_ARGUMENTS
-void B_PLUS_TREE_INTERNAL_PAGE_TYPE::SetKeyAt(int index, const KeyType &key) {}
+void B_PLUS_TREE_INTERNAL_PAGE_TYPE::SetKeyAt(int index, const KeyType &key) {
+  BUSTUB_ASSERT( index >= 1 && index < GetSize(), "index out of range at SetKeyAt");
+  array_[index].first = key;
+}
 
 /*
  * Helper method to get the value associated with input "index"(a.k.a array
  * offset)
  */
 INDEX_TEMPLATE_ARGUMENTS
-auto B_PLUS_TREE_INTERNAL_PAGE_TYPE::ValueAt(int index) const -> ValueType { return 0; }
+auto B_PLUS_TREE_INTERNAL_PAGE_TYPE::ValueAt(int index) const -> ValueType {
+  BUSTUB_ASSERT( index >= 0 && index < GetSize(), "index out of range at ValueAt");
+  return array_[index].second;
+}
+
+INDEX_TEMPLATE_ARGUMENTS
+void B_PLUS_TREE_INTERNAL_PAGE_TYPE::SetValueAt(int index, const ValueType &value) {
+  BUSTUB_ASSERT( index >= 0 && index < GetSize(), "index out of range at SetValueAt");
+  array_[index].second = value;
+}
 
 // valuetype for internalNode should be page id_t
 template class BPlusTreeInternalPage<GenericKey<4>, page_id_t, GenericComparator<4>>;
